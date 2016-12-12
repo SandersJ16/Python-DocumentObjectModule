23742f1697ecd8645fc6ac4dafde0c0cb96f39fe
sandersj16 <justin@pointw.com>
2016-12-12 02:26:59 -0600

    Modified Tests to be a bit more generic about attribute order

--- TestDOM/TestDOMMethods.py
+++ TestDOM/TestDOMMethods.py
@@ -3,52 +3,111 @@ from DOM import XmlMaker
 
 
 class TestXmlMakerMethods(unittest.TestCase):
+    attribute_regex = r"(\s\w+=\".*\")"
 
     def test_renderSingleTag(self):
-        xml = XmlMaker("table")
-        expected_xml = "<table></table>"
-        self.assertEqual(xml.__str__(), expected_xml)
+        tag_name = "table"
+
+        xml = XmlMaker(tag_name)
+        expected_xml = r"<{tag_name}></{tag_name}>".format(tag_name=tag_name)
+
+        self.assertEqual(xml.__str__(), expected_xml);
 
     def test_renderTagWithSingleAttributes(self):
-        xml = XmlMaker("element").addAttribute("id", "test")
-        expected_xml = "<element id=\"test\"></element>"
+        tag_name = "element"
+        attribute = "id"
+        value = "test"
+
+        xml = XmlMaker(tag_name).addAttribute(attribute, value)
+        expected_xml = r'<{tag_name} {attribute}="{value}"></{tag_name}>'.format(tag_name=tag_name,
+                                                                                 attribute=attribute,
+                                                                                 value=value)
+
         self.assertEqual(xml.__str__(), expected_xml)
 
     def test_renderTagWithMultipleAttributes(self):
-        xml = XmlMaker("element").addAttribute("id", "test")
-        xml.addAttribute("name", "some_element")
-        expected_xml = "<element id=\"test\" name=\"some_element\"></element>"
-        self.assertEqual(xml.__str__(), expected_xml)
+        tag_name = "element"
+        attributes = {"id"  : "test",
+                      "name": "some_element"}
+
+        xml = XmlMaker(tag_name)
+
+        for attribute, value in attributes.items():
+            xml.addAttribute(attribute, value)
+
+        for attribute, value in attributes.items():
+            expected_xml = r"<{tag_name}{attribute_regex}* {attribute}=\"{value}\"{attribute_regex}*></{tag_name}>".format(tag_name=tag_name,
+                                                                                                                           attribute=attribute,
+                                                                                                                           value=value,
+                                                                                                                           attribute_regex=self.attribute_regex)
+            self.assertRegex(xml.__str__(), expected_xml)
 
     def test_renderTagWithSingleAttributeAssignedMultipleTimes(self):
-        xml = XmlMaker("form").addAttribute("class", "class_name")
-        xml.addAttribute("class", "default_class")
-        expected_xml = "<form class=\"class_name default_class\"></form>"
-        self.assertEqual(xml.__str__(), expected_xml)
+        tag_name = "form"
+        attribute = "class"
+        values = ["class_name", "default_class"]
+
+
+        xml = XmlMaker(tag_name)
+        for value in values:
+            xml.addAttribute(attribute, value)
+
+        for value in values:
+            expected_xml = r'<{tag_name} {attribute}="(\w+ )?{value}( \w+)?"></{tag_name}>'.format(tag_name=tag_name,
+                                                                                                   attribute=attribute,
+                                                                                                   value=value)
+            self.assertRegex(xml.__str__(), expected_xml)
 
     def test_overwritingAnAttribute(self):
-        xml = XmlMaker("book").addAttribute("genre", "fantasy").addAttribute("genre", "drama")
-        xml.setAttribute("genre", "sci-fi")
-        expected_xml = "<book genre=\"sci-fi\"></book>"
+        tag_name = "book"
+        attribute = "genre"
+        initial_value = "fantasy"
+        overwrite_value = "sci-fi"
+
+        xml = XmlMaker(tag_name).addAttribute(attribute, initial_value).setAttribute(attribute, overwrite_value)
+        expected_xml = r'<{tag_name} {attribute}="{value}"></{tag_name}>'.format(tag_name=tag_name,
+                                                                                 attribute=attribute,
+                                                                                 value=overwrite_value)
+
         self.assertEqual(xml.__str__(), expected_xml)
 
     def test_renderTagWithChildren(self):
-        xml = XmlMaker("table").addChild(XmlMaker("tr"))
-        expected_xml = ("<table><tr></tr></table>")
+        parent_tag = "table"
+        child_tag = "tr"
+        xml = XmlMaker(parent_tag).addChild(XmlMaker(child_tag))
+        expected_xml = r"<{parent_tag}><{child_tag}></{child_tag}></{parent_tag}>".format(parent_tag=parent_tag,
+                                                                                          child_tag=child_tag)
         self.assertEqual(xml.__str__(), expected_xml)
 
     def test_renderTagWithContent(self):
-        xml = XmlMaker("speech").addContent("I have a dream")
-        expected_xml = "<speech>I have a dream</speech>"
+        tag_name = "speech"
+        content = "I have a dream"
+
+        xml = XmlMaker(tag_name).addContent(content)
+        expected_xml = r"<{tag_name}>{content}</{tag_name}>".format(tag_name=tag_name,
+                                                                    content=content)
         self.assertEqual(xml.__str__(), expected_xml)
 
     def test_manualSortOrders(self):
-        xml = XmlMaker("books")
+        parent_tag = "books"
+        child_tag = "author"
+        child_content = "Robert Heinlen"
+        content_before_child = "hope"
+        content_after_child = "test"
+
+
+        xml = XmlMaker(parent_tag)
         xml.sort_order_iterator = 1000
-        xml.addContent("test")
-        xml.addChild(XmlMaker("author").addContent("Robert Hindland"), 500)
-        xml.addContent("hope", 250)
-        expected_xml = "<books>hope<author>Robert Hindland</author>test</books>"
+
+        xml.addContent(content_after_child)
+
+        xml.addChild(XmlMaker(child_tag).addContent(child_content), 500)
+        xml.addContent(content_before_child, 250)
+        expected_xml = r"<{parent_tag}>{first_content}<{child_tag}>{child_content}</{child_tag}>{second_content}</{parent_tag}>".format(parent_tag=parent_tag,
+                                                                                                                                        child_tag=child_tag,
+                                                                                                                                        child_content=child_content,
+                                                                                                                                        first_content=content_before_child,
+                                                                                                                                        second_content=content_after_child)
         self.assertEqual(xml.__str__(), expected_xml)
 
 if __name__ == "__main__":
